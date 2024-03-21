d7eb6b9b316e834939705033b520a7e372f25a37
Duy Quốc <107761696+DuyQuocPhan@users.noreply.github.com>
2024-03-21 22:16:01 +0700

    Create AppInfo.java

---
+++ app/src/main/java/edu/project/TouristTicketOrder/AppInfo.java
@@ -0,0 +1,7 @@
+package edu.project.TouristTicketOrder;
+
+public class AppInfo {
+    public static final int APP_ID = 2554;
+    public static final String MAC_KEY = "PcY4iZIKFCIdgZvA6ueMcMHHUbRLYjPL";
+    public static final String URL_CREATE_ORDER = "https://sb-openapi.zalopay.vn/v2/create";
+}
